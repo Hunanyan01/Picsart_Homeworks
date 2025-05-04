e0d26d12ad7a676472c5f59ed7581aee3c52c7fe
Hunanyan01 <hunanyandavid877@gmail.com>
2025-05-04 13:35:46 +0400

    домашние упражнения

---
+++ src/Lesson1.java
@@ -0,0 +1,13 @@
+public class Lesson1 {
+    public static void main(String[] args) {
+        System.out.println("Hello, Picsart Academy!");
+        System.out.println("Today we are learning Java.");
+        System.out.println("\n");
+        System.out.println("Here are some important things to remember:");
+        System.out.println("- Always write clean code.");
+        System.out.println("- Use proper indentation.");
+        System.out.println("- Comment your code when necessary.");
+        System.out.print("\n\n\n");
+        System.out.println("Good luck and have fun!");
+    }
+}
