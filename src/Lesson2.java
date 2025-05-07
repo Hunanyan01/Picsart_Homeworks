630005c52a8f7ae5a3621c213532c280b74f1f19
Hunanyan01 <hunanyandavid877@gmail.com>
2025-05-07 15:41:13 +0400

    Ավելացրեցի Lesson2.java ֆայլը

---
+++ src/Lesson2.java
@@ -0,0 +1,24 @@
+public class Lesson2 {
+    public static void main(String[] args) {
+
+        byte tariq = 25;
+        short bardzrutyun = 170;
+        int ashxatavardz = 150000;
+        long bnakchutyun = 3000000L;
+        float qash = 65.5f;
+        double srti_hachaxakanutyun = 72.5;
+        char ser = 'M';
+        boolean hay_e = true;
+        String anun ="Heriqnaz";
+
+        System.out.println("Aanun" + "- " + anun);
+        System.out.println("Tariq" + "- " + tariq);
+        System.out.println("Bardzrutyun" + "- "  + bardzrutyun + " sm");
+        System.out.println("Ashxatavardz"  + "- " + ashxatavardz + " dram");
+        System.out.println("Bnakchutyun" + "- " + bnakchutyun);
+        System.out.println("Qash" + "- "  + qash + " kg");
+        System.out.println("Srti Ashxatanqi Hachaxanutyun" + "- "  + srti_hachaxakanutyun + " zark/rope");
+        System.out.println("Ser" + "- "  + ser);
+        System.out.println("Hay e?"  + "- "  + hay_e);
+    }
+}
